4d73c7c6d0c9154e8e4e3f1fed054c9715c8d2b3
Nagendra Reddy <nagendramca2011@gmail.com>
2021-02-28 17:06:12 +0530

    server file updated

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,7 +3,7 @@ const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
 app.use("*",(req, res) =>{
-    res.send(`<h1>Server up and running!`);
+    res.send(`<h1>Server up and running !</h1>`);
 });
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
