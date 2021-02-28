7b51fefca985cec394dd234b70fae8303f756be9
Nagendra Reddy <nagendramca2011@gmail.com>
2021-02-28 17:03:08 +0530

    basic setup application

---
+++ src/config/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express()
+const PORT : string|number = process.env.PORT || 5000;
+
+app.use("*",(req, res) =>{
+    res.send(`<h1>Server up and running!`);
+});
+
+app.listen(PORT,() => console.log(`hosting @${PORT}`));
