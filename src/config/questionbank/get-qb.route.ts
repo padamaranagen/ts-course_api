84604b55472937910db21297c2f63ac3e828fc82
Nagendra Reddy <nagendramca2011@gmail.com>
2021-06-24 18:29:02 +0530

    path modified

--- src/config/questionbank/get-qb.route.ts
+++ src/config/questionbank/get-qb.route.ts
@@ -2,7 +2,7 @@ import {
     Request,
     Response
 } from 'express';
-import { questionbank } from '../config/questionbank/qb.json'
+import { questionbank } from '../questionbank/qb.json'
 export function getQuestionbank(req: Request, res: Response) {
     setTimeout(() => {
 
